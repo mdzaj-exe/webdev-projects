19044ecfa94bcf3c409f828cdf69de6d4e3974e1
AJ.exe <ajwint.cs@gmail.com>
2023-11-14 16:52:31 -0500

    Basic Animate on Scroll w/JS

---
+++ JSProjects/Animate on Scroll/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+//https://www.youtube.com/watch?v=T33NN_pPeNI&t=127s
