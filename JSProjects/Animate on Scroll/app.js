9260c35259cc646868591c1f51529f118503a5ad
AJ.exe <ajwint.cs@gmail.com>
2023-11-22 20:17:31 -0500

    update to animate on scroll

--- JSProjects/Animate on Scroll/app.js
+++ JSProjects/Animate on Scroll/app.js
@@ -11,6 +11,35 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("secret");
+  overlay.classList.remove("secret");
+};
+
+const closeModal = function () {
+  modal.classList.add("secret");
+  overlay.classList.add("secret");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("secret")) {
+    closeModal();
+  }
+});
+
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
