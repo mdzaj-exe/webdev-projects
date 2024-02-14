a277d3fc28dad51a3edc710f9dca912054c831cb
AJ Wint <ajwint.cs@gmail.com>
2024-02-14 00:43:51 -0500

    commented out save function, style updates

--- JSProjects/WordChecker/script.js
+++ JSProjects/WordChecker/script.js
@@ -3,11 +3,32 @@ var wordCheckArray = [];
 function saveArrayToLocalStorage(array) {
   localStorage.setItem("wordCheck", JSON.stringify(array));
 }
+/*
+function saveArrayToFile(array) {
+  // Send POST request to server w/updated array data
+  fetch("/updateArray", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ array }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to save array to file");
+      }
+      console.log("Array saved to file successfully");
+    })
+    .catch((error) => {
+      console.error("Error saving array to file:", error);
+    });
+}
+*/
 
 document.addEventListener("DOMContentLoaded", function () {
   // Waits for DOM content to fully load before attaching EL
 
-  fetch("JSProjectsWordCheckerarray.json")
+  fetch("array.json")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Failed to fetch array from file");
@@ -16,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .then((data) => {
       wordCheckArray = data; // Initialize wordCheckArray with the retrieved array
-
       // Access form and add event listener for the submit event
       document
         .querySelector("#phraseCheck")
@@ -35,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log("The phrase is not in the array.");
             wordCheckArray.push(enteredPhrase);
             saveArrayToLocalStorage(wordCheckArray); // Using "wordCheck" as the key
+            // saveArrayToFile(wordCheckArray);
           }
 
           // Entering phrase into console
